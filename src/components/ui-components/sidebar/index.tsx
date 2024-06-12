32eb08ffbd8863e3511388518d0d5d23d0b80fe3
Muhammad Areeb Vohra <34376767+areebvohra@users.noreply.github.com>
2024-06-13 01:46:26 +0500

    drawer component

--- src/components/ui-components/sidebar/index.tsx
+++ src/components/ui-components/sidebar/index.tsx
@@ -4,8 +4,8 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
-import { Toolbar, List, CssBaseline, Typography, Divider, Collapse, IconButton, Drawer as MuiDrawer, Box, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { Toolbar, List, CssBaseline, Typography, Divider, Collapse, IconButton, Box, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 
 import { ExpandMore, Menu as MenuIcon, ChevronLeft as ChevronLeftIcon } from '@mui/icons-material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -21,47 +21,7 @@ import dkDataEngineerIcon from '../../../Images/icons/data-engr.png';
 import dkProductIcon from '../../../Images/icons/product.png';
 import logo from '../../../Images/leakdtech-logo.png';
 import AppBar from '../appbar';
-import { DrawerHeader } from '../drawer';
-
-const drawerWidth = 240;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  }),
-);
+import { Drawer, DrawerHeader } from '../drawer';
 
 const drawerData = [
   { name: 'Dashboard', src: dashboardIcon, alt: 'dashboardIcon', leftIcon: false },
