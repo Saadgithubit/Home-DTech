b6bd946086c844b4813359ea58055b0e2a8b092b
Muhammad Areeb Vohra <34376767+areebvohra@users.noreply.github.com>
2024-06-21 18:22:51 +0500

    styling fixed

--- src/app/(user-management)/users/page.tsx
+++ src/app/(user-management)/users/page.tsx
@@ -4,15 +4,13 @@ import NavContainer from "@/components/navcontainer";
 
 export default function Users() {
     return (
-        <div className="flex justify-end p-2">
-            <div className="w-[85%]">
+        <div className="w-full mt-14 p-4">
             <span className="w-full inline-flex p-2 text-xl items-center justify-between">
                 <h1>Users</h1>
                 <ButtonComponent variant={'contained'} color={'primary'} title={'Add User'} />
             </span>
             <NavContainer/>
             <InnerHeader title={'Users List'}/>
-            </div>
         </div>
     )
 }
