902f5bb96c5a1afc8d96da68f6c3f22fa38259da
Muhammad Areeb Vohra <34376767+areebvohra@users.noreply.github.com>
2024-06-21 19:52:19 +0500

    styling fix

--- src/components/innerheader/index.tsx
+++ src/components/innerheader/index.tsx
@@ -1,7 +1,7 @@
 export default function InnerHeader({ title }: { title: string }) {
 
     return (
-        <div className="border-2 bg-[#17A2B8] text-white p-3 mb-2 rounded-lg">
+        <div className="bg-[#17A2B8] text-white py-3 px-5 mb-2 rounded-s rounded-e">
             <h1>{title}</h1>
         </div>
     )
