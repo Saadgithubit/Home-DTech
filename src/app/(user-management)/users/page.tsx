311be2f0673125384c96d187df3bd68d0bb42b40
Muhammad Areeb Vohra <34376767+areebvohra@users.noreply.github.com>
2024-06-22 06:22:01 +0500

    userList destructured

--- src/app/(user-management)/users/page.tsx
+++ src/app/(user-management)/users/page.tsx
@@ -1,14 +1,10 @@
-'use client'
-
-import React, { ChangeEvent } from 'react';
-import { Delete as DeleteIcon, Create as CreateIcon } from '@mui/icons-material'
-import { Box, ListItem } from "@mui/material";
-import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import React from 'react';
+import { Box } from "@mui/material";
 
 import ButtonComponent from "@/components/button";
 import InnerHeader from "@/components/innerheader";
 import NavContainer from "@/components/navcontainer";
-import Pagination from '@/components/pagination';
+import UserList from '@/components/pageLists/userList';
 
 export default function Users() {
     const list = [
@@ -22,6 +18,10 @@ export default function Users() {
         { name: "Luke Neale", userName: "Lukeneale.1", email: "Lukeneale.1@gmail.com", role: "User/Landlords" },
         { name: "Anil Kumar", userName: "Anil Kuman", email: "accounts@leakdtech.com", role: "Accounts" },
         { name: "Jonathan Wileman", userName: "jon", email: "jon@leakdtech.com", role: "Engineering Director" },
+        { name: "Jonathan Wileman", userName: "jon", email: "jon@leakdtech.com", role: "Engineering Director" },
+        { name: "Jesmi Latheef", userName: "jasmi.latheef", email: "jasmi@leakdtech.com", role: "Operations" },
+        { name: "Jesmi Latheef", userName: "jasmi.latheef", email: "jasmi@leakdtech.com", role: "Operations" },
+        { name: "Luke Neale", userName: "Lukeneale.1", email: "Lukeneale.1@gmail.com", role: "User/Landlords" },
     ]
 
     return (
@@ -37,73 +37,4 @@ export default function Users() {
             </Box>
         </div>
     )
-}
-
-interface UserListProps {
-    list: ListItem[];
-    itemsPerPage?: number;
-}
-
-interface ListItem {
-    name: string;
-    userName: string;
-    email: string;
-    role: string;
-}
-
-const UserList: React.FC<UserListProps> = ({ list, itemsPerPage = 5 }) => {
-    const router = useRouter();
-    const pathname = usePathname();
-    const searchParams: any = useSearchParams();
-    const currentPage = parseInt(searchParams.get('page')) || 1;
-
-    const handlePageChange = (event: ChangeEvent<unknown>, value: number) => {
-        router.push(`${pathname}?page=${value}`);
-    };
-
-    const lastIndex = currentPage * itemsPerPage;
-    const firstIndex = lastIndex - itemsPerPage;
-    const displayedList = list.slice(firstIndex, lastIndex);
-    const totalPages = Math.ceil(list.length / itemsPerPage);
-
-    return (
-        <div>
-            <div className="pb-4">
-                <Pagination count={totalPages} page={currentPage} onChange={handlePageChange} />
-            </div>
-            <ul className="flex border-b-2 py-4 px-2 text-sm font-semibold space-x-16">
-                <li className='w-28'>Name</li>
-                <li className='w-28'>User Name</li>
-                <li className='w-44'>Email</li>
-                <li className='w-28'>Role</li>
-                <li className='w-36'>Account ID</li>
-                <li className='w-28'>Status</li>
-                <li>Actions</li>
-            </ul>
-            {displayedList.map((item, index) => {
-                const { name, userName, email, role } = item
-                return (
-                    <ul key={index} className="flex border-b-2 py-4 px-2 text-xs space-x-16 items-center">
-                        <li className='w-28'>{name}</li>
-                        <li className='w-28'>{userName}</li>
-                        <li className='w-44'>{email}</li>
-                        <li className='w-28'>{role}</li>
-                        <li className='w-36'></li>
-                        <li className='w-28 px-3'><span className="inline-block w-3 h-3 rounded-full bg-[#28A745]"></span></li>
-                        <li className='flex'>
-                            <button className="p-2 rounded-md text-white bg-[#0069D9] border-2">
-                                <CreateIcon />
-                            </button>
-                            <button className="p-2 rounded-md text-white bg-[#C82333] border-2">
-                                <DeleteIcon />
-                            </button>
-                        </li>
-                    </ul>
-                )
-            })}
-            <div className="pt-4">
-                <Pagination count={totalPages} page={currentPage} onChange={handlePageChange} />
-            </div>
-        </div>
-    );
 }
