4c1019af7fca3f620b777afea88b490380459c97
Saadgithubit <saad45257@gmail.com>
2024-06-29 02:16:10 +0500

    Add Inputs & select

--- src/app/(user-management)/users/add/page.tsx
+++ src/app/(user-management)/users/add/page.tsx
@@ -1,26 +1,104 @@
-import { Box, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+'use client'
+import ButtonComponent from "@/components/button";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Box, FormControl, IconButton, InputAdornment, InputLabel, MenuItem, OutlinedInput, Select, TextField } from "@mui/material";
+import { useState } from "react";
 
 export default function AddUser() {
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
         <div>
             <h1 className="text-3xl font-sans mt-2">Users</h1>
             <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 2, display: 'flex', flexWrap: 'wrap', gap: '15px' }}>
-                <TextField sx={{ width: '24%' }} required id="outlined-required" label="Name" type="text" size="small" />
-                <TextField sx={{ width: '24%' }} required id="outlined-required" label="UserName" type="text" size="small" />
-                <TextField sx={{ width: '24%' }} required id="outlined-required" label="Phone" type="number" size="small" />
-                <TextField sx={{ width: '24%' }} required id="outlined-required" label="Email" type="email" size="small" />
-                <FormControl sx={{width: '24%'}}>
-                    <InputLabel id="demo-simple-select-label">Status</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        label="Status"
-                        size="small"
-                    >
-                        <MenuItem value='Select'>Select</MenuItem>
-                        <MenuItem value='Active'>Active</MenuItem>
-                        <MenuItem value='Inactive'>Inactive</MenuItem>
+                <FormControl sx={{ width: '24%', gap: '10px' }}>
+                    <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
+                        Name:
+                    </span>
+                    <OutlinedInput size="small" type="text" placeholder="Name" />
+                </FormControl>
+                <FormControl sx={{ width: '24%', gap: '10px' }}>
+                    <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
+                        UserName:
+                    </span>
+                    <OutlinedInput size="small" type="text" placeholder="Name" />
+                </FormControl>
+                <FormControl sx={{ width: '24%', gap: '10px' }}>
+                    <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
+                        Phone:
+                    </span>
+                    <OutlinedInput size="small" type="number" placeholder="Phone" />
+                </FormControl>
+                <FormControl sx={{ width: '24%', gap: '10px' }}>
+                    <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
+                        Email:
+                    </span>
+                    <OutlinedInput size="small" type="email" placeholder="Email" />
+                </FormControl>
+                <FormControl sx={{ width: '24%', gap: '10px' }}>
+                <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
+                        Roles:
+                    </span>
+                    <Select size='small' value='Select'>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
                     </Select>
+                </FormControl>
+                <FormControl sx={{ width: '24%', gap: '10px' }}>
+                <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
+                        Status:
+                    </span>
+                    <Select size='small' value='Select'>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+                <FormControl sx={{ width: '24%', gap: '10px' }}>
+                    <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
+                        Password:
+                    </span>
+                    <OutlinedInput
+                        size="small"
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder="Password"
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+
+                    />
+
+                </FormControl>
+                <FormControl sx={{ width: '24%', gap: '10px' }}>
+                    <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
+                        Confirm Password:
+                    </span>
+                    <OutlinedInput
+                        size="small"
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder="Confirm Password"
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+
+                    />
                 </FormControl>
             </Box>
         </div>
