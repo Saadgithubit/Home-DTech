b199767dfbd1989de068849fbdb1e1d4e23fd333
Muhammad Areeb Vohra <34376767+areebvohra@users.noreply.github.com>
2024-06-28 20:36:20 +0500

    filter section redesign

--- src/components/filtersection/index.tsx
+++ src/components/filtersection/index.tsx
@@ -1,25 +1,21 @@
+import { Box, TextField } from "@mui/material";
 import ButtonComponent from "../button";
 
 export default function FilterSection() {
     return (
-        <div className="flex justify-between bg-[#F8F9FA] rounded-md pt-4 px-2 my-2 border-2 flex-wrap">
-            <span className="inline-flex flex-col space-y-2 w-1/3">
-                <p className="text-xs font-semibold ml-1">Users:</p>
-                <select className="border-2 text-xs w-[90%] p-2">
-                    <option>Select</option>
-                </select>
-            </span>
-            <span className="inline-flex flex-col space-y-2 w-1/3">
-                <p className="text-xs font-semibold ml-1">Name:</p>
-                <input placeholder="Name" className="border-2 text-xs w-[90%] p-2" />
-            </span>
-            <span className="inline-flex flex-col space-y-2 w-1/3">
-                <p className="text-xs font-semibold ml-1">Address:</p>
-                <input placeholder="Address" className="border-2 text-xs w-[90%] p-2" />
-            </span>
-            <span className="w-full justify-end inline-flex pr-9 p-2 my-2 space-x-2">
+        <div className="flex flex-col bg-[#F8F9FA] rounded-md p-3 my-2 border-2">
+            <Box
+                component="form"
+                sx={{ '& > :not(style)': { mb: 1, width: '100%' }, display: 'flex', flexDirection: 'row' }}
+                noValidate autoComplete="off"
+            >
+                <TextField fullWidth label="Name" id="name-input" variant="outlined" color="primary" />
+                <TextField fullWidth label="Email" id="email-input" variant="outlined" className="mx-3" />
+                <TextField fullWidth label="Address" id="address-input" variant="outlined" />
+            </Box>
+            <div className="w-full justify-end inline-flex">
                 <ButtonComponent title={'Reset'} />
-            </span>
+            </div>
         </div>
     )
 }
