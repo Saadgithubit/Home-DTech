174079f590494af9ff29c8a4fb776699bd68f627
Muhammad Areeb Vohra <34376767+areebvohra@users.noreply.github.com>
2024-06-29 14:39:51 +0500

    eslint added

--- src/app/building/page.tsx
+++ src/app/building/page.tsx
@@ -4,7 +4,6 @@ import NavContainer from "@/components/filtersection";
 import { Box } from "@mui/material";
 import BuildingList from "@/components/pageLists/buildingLists";
 
-
 export default function Building() {
     const list = [
         { image: 'https://www.shutterstock.com/image-photo/driveway-view-stone-facade-house-600nw-2207752337.jpg', user: 'Damion Lock', name: 'House 2 of Damion', type: 'Villa', city: 'Dubai', address: 'Villa 269, Primrose, Akoya, Damac Hills 2', width: 12, length: 25, height: 12, area: 0, },
