0d2cf0c9698053c461786392633d780d5c5d9adf
Muhammad Areeb Vohra <34376767+areebvohra@users.noreply.github.com>
2024-06-29 14:37:21 +0500

    .

--- src/app/building/page.tsx
+++ src/app/building/page.tsx
@@ -18,6 +18,7 @@ export default function Building() {
         { image: 'https://www.shutterstock.com/image-photo/driveway-view-stone-facade-house-600nw-2207752337.jpg', user: 'Damion Lock', name: 'House 2 of Damion', type: 'Villa', city: 'Dubai', address: 'Villa 269, Primrose, Akoya, Damac Hills 2', width: 12, length: 25, height: 12, area: 0, },
         { image: 'https://www.shutterstock.com/image-photo/driveway-view-stone-facade-house-600nw-2207752337.jpg', user: 'Damion Lock', name: 'House 2 of Damion', type: 'Villa', city: 'Dubai', address: 'Villa 269, Primrose, Akoya, Damac Hills 2', width: 12, length: 25, height: 12, area: 0, },
     ]
+
     return (
         <div>
             <span className="flex p-2 text-xl items-center justify-between">
