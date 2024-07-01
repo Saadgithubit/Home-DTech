4301b481057670db4a90770c803753a33440c350
Muhammad Areeb Vohra <34376767+areebvohra@users.noreply.github.com>
2024-07-01 16:20:00 +0500

    unnecessary

--- src/components/homebar/index.tsx
+++ src/components/homebar/index.tsx
@@ -1,16 +1,9 @@
-'use client'
+export default function Homebar() {
 
-import { DrawerContext } from "@/contextApi"
-import { useContext } from "react"
-
-export default function Homebar(){
-    let { isOpen } = useContext(DrawerContext)
-    // console.log(open);
-    
-    return(
-            <div>
-            <h1 className="text-2xl my-4">Home</h1>
+    return (
+        <div className="">
+            <h1 className="text-2xl py-2">Home</h1>
             <span className="shadow-lg inline-block p-3 w-full bg-[#FFFFFF]">Welcome to Home DTech</span>
-            </div>
+        </div>
     )
 }
