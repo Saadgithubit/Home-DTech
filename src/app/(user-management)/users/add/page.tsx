e305ecf76fdb7d49ea0f49acecde383246ecb4d5
Saadgithubit <saad45257@gmail.com>
2024-07-01 20:25:28 +0500

    users page complete

--- src/app/(user-management)/users/add/page.tsx
+++ src/app/(user-management)/users/add/page.tsx
@@ -1,15 +1,17 @@
 'use client'
 import ButtonComponent from "@/components/button";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { Box, FormControl, IconButton, InputAdornment, InputLabel, MenuItem, OutlinedInput, Select, TextField } from "@mui/material";
+import { Box, Button, FormControl, IconButton, InputAdornment, InputLabel, MenuItem, OutlinedInput, Select, TextField } from "@mui/material";
 import { useState } from "react";
 
 export default function AddUser() {
     const [showPassword, setShowPassword] = useState(false);
+    const [roles, setroles] = useState<string>()
+    const [status, setstatus] = useState<string>()
 
     return (
-        <div className="p-2">
-            <h1 className="text-3xl font-sans mt-2">Users</h1>
+        <div className="p-4">
+            <h1 className="text-3xl my-4 font-sans mt-2">Users</h1>
             <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 2, display: 'flex', flexWrap: 'wrap', gap: '15px' }}>
                 <FormControl sx={{ width: '24%', gap: '10px' }}>
                     <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
@@ -21,7 +23,7 @@ export default function AddUser() {
                     <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
                         UserName:
                     </span>
-                    <OutlinedInput size="small" type="text" placeholder="Name" />
+                    <OutlinedInput size="small" defaultValue='areeb.vohra' type="text" placeholder="Name" />
                 </FormControl>
                 <FormControl sx={{ width: '24%', gap: '10px' }}>
                     <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
@@ -39,7 +41,8 @@ export default function AddUser() {
                     <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
                         Roles:
                     </span>
-                    <Select size='small' value='Select'>
+                    <Select size='small' value={roles} onChange={(e) => setroles(e.target.value)}>
+                        <OutlinedInput size="small" type="search" sx={{ width: '100%' }} />
                         <MenuItem value={10}>Ten</MenuItem>
                         <MenuItem value={20}>Twenty</MenuItem>
                         <MenuItem value={30}>Thirty</MenuItem>
@@ -49,10 +52,10 @@ export default function AddUser() {
                     <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xs font-bold text-slate-700">
                         Status:
                     </span>
-                    <Select size='small' value='Select'>
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
+                    <Select size='small' value={status} onChange={(e) => setstatus(e.target.value)}>
+                        <MenuItem value={10}>Select</MenuItem>
+                        <MenuItem value={20}>Active</MenuItem>
+                        <MenuItem value={30}>Inactive</MenuItem>
                     </Select>
                 </FormControl>
                 <FormControl sx={{ width: '24%', gap: '10px' }}>
@@ -60,6 +63,7 @@ export default function AddUser() {
                         Password:
                     </span>
                     <OutlinedInput
+                        defaultValue='qwerty1234'
                         size="small"
                         type={showPassword ? 'text' : 'password'}
                         placeholder="Password"
@@ -101,6 +105,14 @@ export default function AddUser() {
                     />
                 </FormControl>
             </Box>
+            <span className="flex shadow-lg space-x-2 p-4">
+                <ButtonComponent title={'Create'} />
+                <Box sx={{ '& button': { px: 2.5, py: 1, fontSize: 12, fontWeight: 'bold', background: '#5A6268' } }}>
+                    <Button size="small" variant={'contained'}>
+                        Cancel
+                    </Button>
+                </Box>
+            </span>
         </div>
     )
 }
