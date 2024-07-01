72c4ed440e2b81c0e69f679898f41cc5b5279aed
Muhammad Areeb Vohra <34376767+areebvohra@users.noreply.github.com>
2024-07-01 16:19:13 +0500

    unnecessary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Homebar from "@/components/homebar";
 
-export default function Home() {
+export default async function Home() {
+  const delay = (s: number | undefined) => new Promise(resolve => setTimeout(resolve, s))
+  await delay(2000)
+
   return (
     <Homebar />
   );
