09242a5919dc43385cca6c991fee7d3df0ece8c9
Muhammad Areeb Vohra <34376767+areebvohra@users.noreply.github.com>
2024-07-01 15:31:10 +0500

    minor changes

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -7,7 +7,7 @@ interface ButtonComponentProps {
 export default function ButtonComponent({ title }: ButtonComponentProps) {
 
     return (
-        <Box sx={{ '& button': { p: 1, fontSize: 12, fontWeight: 'bold', background: 'primary' } }}>
+        <Box sx={{ '& button': { px: 2.5, py: 1, fontSize: 12, fontWeight: 'bold' } }}>
             <Button size="small" variant={'contained'}>
                 {title}
             </Button>
