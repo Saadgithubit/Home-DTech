3f1a0366a6ea2347e729ef4652e1e9521cb31ca9
Muhammad Areeb Vohra <34376767+areebvohra@users.noreply.github.com>
2024-07-01 16:17:35 +0500

    color changes

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,14 @@
+import { Box, LinearProgress } from "@mui/material";
+
 export default function Loading() {
-    return <p>Loading Data...</p>;
+    return (
+        <Box sx={{ width: '100%' }}>
+            <LinearProgress sx={{
+                backgroundColor: 'white',
+                '& .MuiLinearProgress-bar': {
+                    backgroundColor: '#1976d294'
+                }
+            }} />
+        </Box>
+    )
 }
